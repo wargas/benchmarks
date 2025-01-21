a8c22bb5aff6df2c58929ad152bb561411dde2cf
wargas <teixeira.wargas@gmail.com>
2025-01-21 13:43:53 -0300

    routes

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,13 +1,26 @@
 <?php
 
 use App\Models\User;
+use Database\Factories\UserFactory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(Request $request) {
-    $user = User::where('name', 'wargas');
-    return User::all();
+    return User::limit(10)->get();
 });
+
+Route::get('/insert', function(Request $request) {
+    return User::factory(1)->create();
+});
+
+Route::get('/count', function(Request $request) {
+    return User::count("*");
+});
+
+Route::get('/reset', function(Request $request) {
+    return User::where('id', '>=', 1)->delete();
+});
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
